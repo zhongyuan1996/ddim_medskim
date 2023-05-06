602700942421704f8b5eb0e5246fbc90f38de908
lab <zhongyuan1996@hotmail.com>
2023-05-06 11:14:11 -0400

    diff ARF

--- GAN_bash.sh
+++ GAN_bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-model=("Dipole_ehrGAN" "Dipole_GcGAN" "Dipole_actGAN" "Dipole_medGAN" "TLSTM_ehrGAN" "TLSTM_GcGAN" "TLSTM_actGAN" "TLSTM_medGAN" "SAND_ehrGAN" "SAND_GcGAN" "SAND_actGAN" "SAND_medGAN" )
+model=("LSTM_ehrGAN" "LSTM_GcGAN" "LSTM_actGAN" "LSTM_medGAN" "Dipole_ehrGAN" "Dipole_GcGAN" "Dipole_actGAN" "Dipole_medGAN" "TLSTM_ehrGAN" "TLSTM_GcGAN" "TLSTM_actGAN" "TLSTM_medGAN" "SAND_ehrGAN" "SAND_GcGAN" "SAND_actGAN" "SAND_medGAN" )
 #target_disease=("mimic" "Heart_failure" "COPD" "Kidney" "Amnesia")
-target_disease=("mimic" "ARF" "Shock" "mortality" "Kidney" "Amnesia")
-seeds=(1234 2345 3456)
+target_disease=("mortality")
+seeds=(1234)
 save_path="./saved_models/"
 
 for SEED in ${seeds[@]}
@@ -12,17 +12,8 @@ for SEED in ${seeds[@]}
         do
         for DISEASE in ${target_disease[@]}
         do
-          if [ DISEASE=="mimic" ]; then
-              CUDA_VISIBLE_DEVICES=2, python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=15 --n_epochs=30 -bs=64
-          elif [ DISEASE=="ARF" ]; then
-              CUDA_VISIBLE_DEVICES=2, python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=12 --max_num_codes=5132 --n_epochs=30 -bs=64
-          elif [ DISEASE=="Shock" ]; then
-              CUDA_VISIBLE_DEVICES=2, python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=12 --max_num_codes=5795 --n_epochs=30 -bs=64
-          elif [ DISEASE=="mortality" ]; then
-              CUDA_VISIBLE_DEVICES=2, python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=48 --max_num_codes=7727 --n_epochs=30 -bs=64
-          else
-              CUDA_VISIBLE_DEVICES=2, python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=50 --n_epochs=30 -bs=64
-          fi
+          python3 ALL_GAN_runner.py --seed=$SEED --model=$MODAL --target_disease=$DISEASE --max_len=48 --max_num_codes=7727 --n_epochs=30 -bs=64
+
       done
       done
       done
