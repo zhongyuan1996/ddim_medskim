e670cdf055d36d9925bd4f537ef3b2cacd5e4a58
lab <zhongyuan1996@hotmail.com>
2023-05-06 14:10:05 -0400

    diff ARF

--- ALL_GAN_runner.py
+++ ALL_GAN_runner.py
@@ -473,7 +473,7 @@ def train(args):
 
 if __name__ == '__main__':
     model_name = ['LSTM_ehrGAN', 'LSTM_GcGAN', 'LSTM_actGAN', 'LSTM_medGAN', 'SAND_ehrGAN', 'SAND_GcGAN', 'SAND_actGAN', 'SAND_medGAN', 'TLSTM_ehrGAN', 'TLSTM_GcGAN', 'TLSTM_actGAN', 'TLSTM_medGAN',
-                                                                   'Dipole_ehrGAN', 'Dipole_GcGAN', 'Dipole_actGAN', 'Dipole_medGAN' ]
+                  'Dipole_ehrGAN', 'Dipole_GcGAN', 'Dipole_actGAN', 'Dipole_medGAN' ]
     for name in model_name:
 
         main(name)
