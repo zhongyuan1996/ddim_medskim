ddffbf90e65a23cfd9411a2984c8f691de3e126f
AMD <zhongyuan1996@hotmail.com>
2023-08-09 21:17:22 -0400

    rebuttal

--- models/tabddpm.py
+++ models/tabddpm.py
@@ -22,23 +22,33 @@ class tabDDPM(nn.Module):
 
         self.mlpEncoder = nn.Sequential(
             nn.Linear(self.x_dim, int(self.x_dim / 2)),
-            nn.ReLU(),
+            nn.BatchNorm1d(int(self.x_dim / 2)),
+            nn.LeakyReLU(),
             nn.Dropout(self.dropout),
-            nn.Linear(int(self.x_dim / 2), self.h_dim),
-            nn.ReLU(),
-            nn.Dropout(self.dropout)
+            nn.Linear(int(self.x_dim / 2), int(self.x_dim / 4)),
+            nn.BatchNorm1d(int(self.x_dim / 4)),
+            nn.LeakyReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(int(self.x_dim / 4), self.h_dim),
+            nn.LeakyReLU()
         )
+
         self.mlpDecoder = nn.Sequential(
-            nn.Linear(self.h_dim, int(self.x_dim / 2)),
-            nn.ReLU(),
+            nn.Linear(self.h_dim, int(self.x_dim / 4)),
+            nn.BatchNorm1d(int(self.x_dim / 4)),
+            nn.LeakyReLU(),
+            nn.Linear(int(self.x_dim / 4), int(self.x_dim / 2)),
+            nn.BatchNorm1d(int(self.x_dim / 2)),
+            nn.LeakyReLU(),
             nn.Linear(int(self.x_dim / 2), self.x_dim),
-            nn.ReLU()
+            nn.LeakyReLU()
         )
 
         self.pos_embedding = nn.Embedding(1000+1, x_dim, padding_idx=-1)
 
     def forward(self, x):
         x = self.initial_embedding(x)
+        bs, seq_len, code_len, x_dim = x.shape
 
         diffusion_time_t = torch.randint(0, 1000, (x.shape[0],), device=self.device)
 
@@ -55,26 +65,47 @@ class tabDDPM(nn.Module):
 
         x_diffinput = x_with_noise + time_embedding
 
+        x_diffinput = x_diffinput.view(-1, x_dim)
+
         encoded = self.mlpEncoder(x_diffinput)
         predicted_noise = self.mlpDecoder(encoded)
+
+        predicted_noise = predicted_noise.view(bs, seq_len, code_len, x_dim)
+        x_diffinput = x_diffinput.view(bs, seq_len, code_len, x_dim)
 
         gen_x = x_diffinput - predicted_noise
 
         return x, gen_x
+
 
 class LSTM_predictor(nn.Module):
 
     def __init__(self, vocab_size, x_dim, h_dim, dropout):
         super(LSTM_predictor, self).__init__()
         self.vocab_size = vocab_size
+        self.x_dim = x_dim
+        self.h_dim = h_dim
+
         self.initial_embedding = nn.Embedding(self.vocab_size + 1, x_dim, padding_idx=-1)
         self.lstm = nn.LSTM(x_dim, h_dim, dropout=dropout, batch_first=True)
-        self.linear = nn.Linear(h_dim, 2)
+
+        self.fc1 = nn.Linear(h_dim, int(h_dim / 2))
+        self.bn1 = nn.BatchNorm1d(int(h_dim / 2))
+        self.relu1 = nn.LeakyReLU()
+
+        self.fc2 = nn.Linear(int(h_dim / 2), 2)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         x = self.initial_embedding(x).sum(-2)
         output, (h_n, c_n) = self.lstm(x)
-        output = self.linear(output[:, -1, :])
 
+        f = self.fc1(output[:, -1, :])
+        f = self.bn1(f)
+        f = self.relu1(f)
+        f = self.dropout(f)
+
+        output = self.fc2(f)
         return output
+
 
