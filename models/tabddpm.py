8d0e5e2865a7f0339e2b7278a010e99c61ed8b72
AMD <zhongyuan1996@hotmail.com>
2023-08-20 22:40:14 -0400

    tabddpm updated

--- models/tabddpm.py
+++ models/tabddpm.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from utils.diffUtil import get_beta_schedule
+from models.unet import *
 
 class tabDDPM(nn.Module):
 
@@ -49,37 +50,6 @@ class tabDDPM(nn.Module):
 
         self.pos_embedding = nn.Embedding(1000+1, self.vocab_size+1, padding_idx=-1)
 
-    # def old_forward(self, x):
-    #     x = self.initial_embedding(x)
-    #     bs, seq_len, code_len, x_dim = x.shape
-    #
-    #     diffusion_time_t = torch.randint(0, 1000, (x.shape[0],), device=self.device)
-    #
-    #     time_embedding = self.pos_embedding(diffusion_time_t)
-    #     time_embedding = time_embedding.unsqueeze(1).unsqueeze(2)
-    #     time_embedding = time_embedding.expand(-1, x.shape[1], x.shape[2], -1)
-    #
-    #
-    #     alpha = (1 - self.betas).cumprod(dim=0).index_select(0, diffusion_time_t).view(-1, 1, 1, 1)
-    #
-    #     normal_noise = torch.randn_like(x)
-    #
-    #     x_with_noise = x * alpha.sqrt() + normal_noise * (1.0 - alpha).sqrt()
-    #
-    #     x_diffinput = x_with_noise + time_embedding
-    #
-    #     x_diffinput = x_diffinput.view(-1, x_dim)
-    #
-    #     encoded = self.mlpEncoder(x_diffinput)
-    #     predicted_noise = self.mlpDecoder(encoded)
-    #
-    #     predicted_noise = predicted_noise.view(bs, seq_len, code_len, x_dim)
-    #     x_diffinput = x_diffinput.view(bs, seq_len, code_len, x_dim)
-    #
-    #     gen_x = x_diffinput - predicted_noise
-    #
-    #     return x, gen_x
-
     def forward(self, x, label):
         x = self.initial_embedding(x).sum(dim=-2)
         bs, seq_len, code_len = x.shape
@@ -101,6 +71,65 @@ class tabDDPM(nn.Module):
         x_diffinput = x_diffinput.view(-1, code_len)
 
         # Encoder and Decoder passes
+        encoded = self.mlpEncoder(x_diffinput)
+        predicted_noise = self.mlpDecoder(encoded)
+
+        # Reshape to original dimensions
+        predicted_noise = predicted_noise.view(bs, seq_len, code_len)
+        x_diffinput = x_diffinput.view(bs, seq_len, code_len)
+
+        gen_x = x_diffinput - predicted_noise
+
+        return x, gen_x
+
+class DDPM(nn.Module):
+
+    def __init__(self, config, vocab_size, x_dim, h_dim, dropout, code_len, device):
+        super(DDPM,self).__init__()
+        self.x_dim = x_dim
+        self.h_dim = h_dim
+        self.dropout = dropout
+        self.device = device
+        self.config = config
+        self.vocab_size = vocab_size
+        self.code_len = code_len
+        self.initial_embedding = nn.Embedding(self.vocab_size+1, self.vocab_size+1, padding_idx=-1)
+        self.y_embedding = nn.Embedding(2, self.vocab_size+1, padding_idx=-1)
+        betas = get_beta_schedule(beta_schedule=self.config.diffusion.beta_schedule,
+                                  beta_start=self.config.diffusion.beta_start,
+                                  beta_end=self.config.diffusion.beta_end,
+                                  num_diffusion_timesteps=self.config.diffusion.num_diffusion_timesteps)
+        betas = self.betas = torch.from_numpy(betas).float().to(self.device)
+        self.diffusion_num_timesteps = betas.shape[0]
+        self.softmax = nn.Softmax(dim=-1)
+
+        # self.diffusion = UNetModel(in_channels=self.channel, model_channels=128,
+        #                            out_channels=self.channel, num_res_blocks=2,
+        #                            attention_resolutions=[16, ])
+
+        self.pos_embedding = nn.Embedding(1000+1, self.vocab_size+1, padding_idx=-1)
+
+    def forward(self, x, label):
+        x = self.initial_embedding(x).sum(dim=-2)
+        bs, seq_len, code_len = x.shape
+
+        # Diffusion time
+        diffusion_time_t = torch.randint(0, 1000, (bs,), device=self.device)
+        time_embedding = self.pos_embedding(diffusion_time_t)
+        time_embedding = time_embedding.unsqueeze(1).expand(-1, x.shape[1], -1)
+        alpha = (1 - self.betas).cumprod(dim=0).index_select(0, diffusion_time_t).view(-1, 1, 1)
+
+        # Add categorical noise
+        uniform_noise = torch.ones_like(x) / self.vocab_size
+        x_with_noise = alpha * x + (1-alpha) * uniform_noise
+        y_embedding = self.y_embedding(label.to(torch.long)).sum(dim=-2).unsqueeze(1).expand(-1, x.shape[1], -1)
+
+        x_diffinput = x_with_noise + time_embedding + y_embedding
+
+        # Flatten the tensor for MLP processing
+        x_diffinput = x_diffinput.view(-1, code_len)
+
+        # # Encoder and Decoder passes
         encoded = self.mlpEncoder(x_diffinput)
         predicted_noise = self.mlpDecoder(encoded)
 
